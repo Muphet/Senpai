49d682960124d727fd11c571cf40174cafdb2714
Yukine <blazylp@gmail.com>
2018-04-22 21:14:00 +0200

    renamed musiclimitd to limitmusic to be more accurate

--- src/commands/Config/limitMusic.js
+++ src/commands/Config/limitMusic.js
@@ -11,7 +11,7 @@ module.exports = class LimitMusicCommand extends Command {
 	}
 
 	async run(msg, [limit = true]) {
-		await msg.guild.configs.update('music.limited', limit, { action: 'add' });
+		await msg.guild.configs.update('music.limited', limit);
 		return msg.send(`Music is ${limit ? 'now limited' : 'not limited anymore'} to the DJ roles`);
 	}
 };
