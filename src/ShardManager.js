a67139a5133c20b403a64f5c5034e9a7e703391f
Yukine <blazylp@gmail.com>
2018-04-22 04:37:53 +0200

    updated ShardManager

--- src/ShardManager.js
+++ src/ShardManager.js
@@ -6,27 +6,15 @@ const wait = promisify(setTimeout);
 const Manager = new ShardingManager(join(__dirname, 'main.js'),
 	{
 		totalShards: 'auto',
-		respawn: true,
 		token: bottoken
 	});
 // Spawn shards
 const spawn = async () => {
 	await wait(5000);
-	Manager.spawn();
+	Manager.spawn(Manager.totalShards, 5500, false);
 };
 spawn();
 
-Manager.on('launch', shard => {
+Manager.on('shardCreate', shard => {
 	console.log(`Shard spawned with ID ${shard.id}`);
-});
-
-// When the shard sends a message with process.send, this will get it.
-Manager.on('message', (Shard, message) => {
-	console.log('[TO MANAGER]:', require('util').inspect(message));
-	if (!message.b) { Shard.send('Welcome! -manager'); } else {
-		Shard.send({
-			obj: 'hi all',
-			b: message.b
-		});
-	}
 });
