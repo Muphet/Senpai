9c8a185e3955811d80c831d67b723da505edff47
Yukine <blazylp@gmail.com>
2018-04-23 16:30:33 +0200

    fix bot check in give command

--- src/commands/Economy/give.js
+++ src/commands/Economy/give.js
@@ -14,7 +14,7 @@ module.exports = class GiveCommand extends Command {
 	async run(msg, [user, amount]) {
 		if (amount <= 0) throw new EconomyError('Value cant be under or equal to 0');
 		if (msg.author.configs.currency < amount) throw new UsageError('You dont have that much money!');
-		if (msg.author.bot) throw new UsageError('You cannot give bots money!');
+		if (user.bot) throw new UsageError('You cannot give bots money!');
 		if (msg.author.id === user.id) throw new UsageError('You cannot give yourself money!');
 		await msg.author.configs.update('currency', msg.author.configs.currency - amount);
 		await user.configs.update('currency', user.configs.currency + amount);
